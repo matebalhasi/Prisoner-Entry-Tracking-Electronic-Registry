9e8a5697ead75bf47d51841d7ade3c23664f0a5c
matebalhasi <126056836+matebalhasi@users.noreply.github.com>
2025-11-23 13:52:32 +0100

    Connecter
    backend és frontend csatlakozó

--- FEController.py
+++ FEController.py
@@ -1,12 +1,12 @@
 
+#import Connection
 
 from Frontend import MainMenuUI
 
 import os
 #from dotenv import load_dotenv, dotenv_values
 
-class FEController:
-#"""Minimalis kontroller, ami csak elindítja a GUI-t."""
 
-    fomenu_ablak = MainMenuUI.MainMenuUI()
-    fomenu_ablak.run()
+
+fomenu_ablak = MainMenuUI.MainMenuUI()
+fomenu_ablak.run()
